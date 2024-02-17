4e68abeebae72cde23e733e122c45a32b64a1f8d
Hafizur Rahman <99hafizurrahman@gmail.com>
2024-02-17 22:44:01 +0530

    card added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import * as data from "emoji-api";
 const Card: React.FC<{
-  data: { emoji: string; name: string; codepoints: number };
+  data: data.Emoji;
 }> = (props) => {
   return (
     <div
       className=" rounded-md flex flex-col items-center gap-2 text-center shadow-md p-8 hover:shadow-lg dark:hover:transiton dark:bg-slate-900 dark:text-white dark:hover:bg-slate-600 dark:hover:transition "
-      key={props.data.codepoints}
+      //   key={props.data.codePoints}
     >
       <span className="text-6xl ">{props.data.emoji}</span>
       <span className="text-xl capitalize pt-3">{props.data.name}</span>
