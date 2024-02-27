b0d45e3a53bab128a0eff86add89d7d3ccfe4a10
Hafizur Rahman <99hafizurrahman@gmail.com>
2024-02-27 21:12:15 +0530

    hidden added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as data from "emoji-api";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy, faShare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy, faShare } from "@fortawesome/free-solid-svg-icons";
 import "./Card.css";
 
 const Card: React.FC<{
@@ -14,22 +14,23 @@ const Card: React.FC<{
 
   const shareEmoji = () => {
     const shareText = `Check out this emoji: ${props.data.emoji}`;
-    navigator.share({
-      title: "Emoji Share",
-      text: shareText,
-      url: window.location.href
-    })
-    .then(() => console.log("Shared successfully"))
-    .catch((error) => console.error("Error sharing:", error));
+    navigator
+      .share({
+        title: "Emoji Share",
+        text: shareText,
+        url: window.location.href,
+      })
+      .then(() => console.log("Shared successfully"))
+      .catch((error) => console.error("Error sharing:", error));
   };
 
   return (
-    <div
-      className="rounded-md flex flex-col items-center justify-between text-center shadow-md p-8 hover:shadow-lg dark:hover:transiton dark:bg-slate-900 dark:text-white dark:hover:bg-slate-600 dark:hover:transition"
-    >
+    <div className="group rounded-md flex flex-col items-center justify-between text-center shadow-md p-8 hover:shadow-lg dark:hover:transiton dark:bg-slate-900 dark:text-white dark:hover:bg-slate-600 dark:hover:transition">
       <span className="text-6xl">{props.data.emoji}</span>
       <span className="text-xl capitalize pt-3">{props.data.name}</span>
-      <div className="flex gap-4 pt-3">
+
+      {/* Hidden by default, visible on hover */}
+      <div className="hidden group-hover:flex gap-4 pt-3">
         <button className="btn-icon" onClick={copyToClipboard}>
           <FontAwesomeIcon icon={faCopy} />
         </button>
